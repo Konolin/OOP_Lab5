406984c25554b8295da079b19aac9cb467edc953
Konolin <mandru.catalin03@gmail.com>
2023-05-11 15:05:23 +0300

    Repo frame

--- Repository.h
+++ Repository.h
@@ -13,10 +13,23 @@ namespace Repository {
     private:
         vector<Scooter> scooterVector{};
 
+        // adds 10 items to the vector
         void generateDummyData();
 
     public:
         // constructor
         Repo();
+
+        // adds the new scooter in the vector, returns the success of the operation
+        bool addScooter(const Scooter& newScooter);
+
+        // delete scooter from the vector, returns the success of the operation
+        bool deleteScooter(const Scooter& deletedScooter);
+
+        // edits the scooter, returns the success of the operation
+        bool editScooter(Scooter& deletedScooter);
+
+        // returns the scooter vector
+        vector<Scooter> getAll();
     };
 }
