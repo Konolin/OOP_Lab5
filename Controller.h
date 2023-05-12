26ea1bf0f085042d0fa905d3d4e12a0062992a4c
Oinacaru Adrian <127617426+Adrian20040101@users.noreply.github.com>
2023-05-12 23:31:42 +0300

    Update Controller.h

--- Controller.h
+++ Controller.h
@@ -5,11 +5,14 @@
 #include "Repository.h"
 #include "Scooter.h"
 
-
 using std::shared_ptr, Repository::Repo, Domain::Date, Domain::Status;
 
 
 namespace Controller {
+    enum UserRole {
+        customer, admin
+    };
+
     class Ctr {
     private:
         shared_ptr<Repo> repository;
@@ -21,9 +24,14 @@ namespace Controller {
     public:
         // constructor
         Ctr(shared_ptr<Repo> repoObj);
-
         static bool hasThreeLetters(const string &id);
-
-        vector<Scooter> sortedByAgeScooters();
+        void add(const std::string& id, const std::string& model, const Date& commissionDate, int mileage,
+                 const std::string& lastLocation, const Status& status);
+        bool remove(const std::string& id);
+        Scooter& find(const std::string& id);
+        void editMileage(const std::string& id, const int& newMileage);
+        void editLocation(const std::string& id, const std::string& newLastLocation);
+        void editStatus(const std::string& id, Status& newStatus);
+        std::vector<Scooter> get_all();
     };
 }
