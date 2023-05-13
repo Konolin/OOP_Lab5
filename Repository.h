1b8e2d7de8f25c5df7f27f11201c23f311755871
Oinacaru Adrian <127617426+Adrian20040101@users.noreply.github.com>
2023-05-13 22:56:52 +0300

    Update Repository.h

--- Repository.h
+++ Repository.h
@@ -13,23 +13,13 @@ namespace Repository {
     private:
         vector<Scooter> scooterVector{};
 
-        // adds 10 items to the vector
         void generateDummyData();
 
     public:
         // constructor
         Repo();
-
-        // adds the new scooter in the vector, returns the success of the operation
-        bool addScooter(const Scooter& newScooter);
-
-        // delete scooter from the vector, returns the success of the operation
-        bool deleteScooter(const Scooter& deletedScooter);
-
-        // edits the scooter, returns the success of the operation
-        bool editScooter(Scooter& deletedScooter);
-
-        // returns the scooter vector
-        vector<Scooter> getAll();
+        void add(const Scooter& new_scooter);
+        bool remove(const std::string& id);
+        std::vector<Scooter> get_all();
     };
 }
