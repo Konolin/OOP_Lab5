d439be72d1b8ba6440ec4ceab903fb212d47928a
Oinacaru Adrian <127617426+Adrian20040101@users.noreply.github.com>
2023-05-23 17:33:35 +0300

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -12,6 +12,10 @@ void User::addReservedScooter(const Scooter &reservedScooter) {
     reservedScooters.push_back(reservedScooter);
 }
 
+vector<Scooter> User::seeAllReservedScooters() {
+    return reservedScooters;
+}
+
 string User::getType() const { return userType; }
 
 User::User() { userType = "NONE"; }
